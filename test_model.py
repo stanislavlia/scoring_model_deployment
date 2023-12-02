e0f3cb43589f8a2c6ffcb696b8cdeaecec99233b
stanislavlia <stanislavliashkov@gmail.com>
2023-12-02 23:47:04 +0700

    started from scratch

---
+++ test_model.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+from catboost import CatBoostClassifier
+
+
+
+test_data = pd.read_csv("prep_golden_test_data.csv").drop("Unnamed: 0", axis=1)
+
+
+model = CatBoostClassifier()
+model.load_model("catboost_cred_scoring.cbm")
+
+print("Model loaded")
+predictions = model.predict_proba(test_data)[:, 1]
+print(predictions[:100])
